e528bcb22c670ecce83ef098903df89ed6652852
Tayyab <tayyab.javed@voneus.com>
2025-03-12 08:14:44 +0000

    demo progesses

--- resources/js/Pages/maeplet/demo.tsx
+++ resources/js/Pages/maeplet/demo.tsx
@@ -2,35 +2,75 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, usePage, Link } from '@inertiajs/react';
 import { useState, useEffect,useCallback } from 'react';
 
-import { ReactFlow, Background, Controls } from '@xyflow/react';
+import { ReactFlow, Background, Controls,applyNodeChanges,applyEdgeChanges,addEdge, MiniMap } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
-const edges = [{ id: '1-2', source: '1', label:'sleep', target: '2' }];
-
-const nodes = [
+const initialNodes = [
     {
-      id: '1', // required
-      position: { x: 0, y: 0 }, // required
-      data: { label: 'launch browser' }, // required
+      id: '1',
+      type: 'input',
+      data: { label: 'Input Node' },
+      position: { x: 250, y: 25 },
+    },
+
+    {
+      id: '2',
+      // you can also pass a React component as a label
+      data: { label: <div>Default Node</div> },
+      position: { x: 100, y: 125 },
     },
     {
-        id: '2',
-        position: { x: 100, y: 100 },
-        data: { label: 'sleep' },
-      },
-      {
-        id: '3',
-        position: { x: 100, y: 100 },
-        data: { label: 'fetch html with AI' },
-      }
+      id: '3',
+      type: 'output',
+      data: { label: 'Output Node' },
+      position: { x: 250, y: 250 },
+    },
   ];
 
+  const initialEdges = [
+    // { id: 'e1-2', source: '1', target: '2' },
+    // { id: 'e2-3', source: '2', target: '3', animated: true },
+  ];
+
+
 const FlowDemo = () => {
+
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+
+    const onNodesChange = useCallback(
+        (changes:any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [setNodes],
+      );
+      const onEdgesChange = useCallback(
+        (changes:any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges],
+      );
+
+      const onConnect = useCallback(
+        (connection:any) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges],
+      );
+
+      const defaultEdgeOptions = { animated: true };
+
+
+
     return (
         <div className='h-screen'>
-        <ReactFlow nodes={nodes} edges={edges}>
+        <ReactFlow
+            draggable={true}
+            nodes={nodes} edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            defaultEdgeOptions={defaultEdgeOptions}
+
+        >
           <Background />
           <Controls />
+          <MiniMap  nodeStrokeWidth={3} zoomable pannable />
+
         </ReactFlow>
       </div>
     )
